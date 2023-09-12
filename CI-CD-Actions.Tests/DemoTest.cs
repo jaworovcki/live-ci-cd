b6209c33d4cc27c434b01d1e070339e06017aabc
Andrii <jaworovcki@gmail.com>
2023-09-12 20:58:29 +0300

    Add integration test

---
+++ CI-CD-Actions.Tests/DemoTest.cs
@@ -0,0 +1,51 @@
+using CI_CD_Actions.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CI_CD_Actions.Tests
+{
+    public class DemoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+
+        [Fact]
+        public async Task CustomerIntegrationTest()
+        {
+            //Create a dbContext
+            var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            var context = new CustomerContext(optionsBuilder.Options);
+
+            //Delete all existing customers
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            //Create Controller
+            var controller = new CustomersController(context);
+
+            //Add a customer
+            await controller.AddCustomer(new Customer()
+            {
+                CustomerName = "FooBar",
+            });
+
+            //Get all customers
+            var customers = await controller.GetCustomers();
+
+            //Assert
+            Assert.Single(customers.Value);
+            Assert.Equal("FooBar", customers.Value.First().CustomerName);
+
+            
+        }
+    }
+}
---
+++ CI-CD-Actions/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI_CD_Actions.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomersController : ControllerBase
+	{
+        private readonly CustomerContext _context;
+
+		public CustomersController(CustomerContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet(Name = nameof(GetCustomers))]
+		public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers() 
+			=> await _context.Customers
+				.AsNoTracking()
+				.ToListAsync();
+
+		[HttpGet("{id}", Name = nameof(GetCustomer))]
+		public async Task<ActionResult<Customer>> GetCustomer(int id)
+		{
+			var customer = await _context.Customers.FindAsync(id);
+
+			if (customer == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(customer);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Customer>> AddCustomer([FromBody] Customer customer)
+		{
+			_context.Customers.Add(customer);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+		}
+
+	}
+}
