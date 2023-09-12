089d63e128a2d803b1dd7af000243f5ee15b7031
Andrii <jaworovcki@gmail.com>
2023-09-12 18:13:12 +0300

    Connect database

---
+++ CI-CD-Actions/Data.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CI_CD_Actions
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+
+        public string CustomerName { get; set; } = string.Empty;
+    }
+
+	public class CustomerContext : DbContext
+	{
+		public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Customer> Customers { get; set; }
+	}
+}
